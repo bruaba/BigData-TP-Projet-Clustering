82c18293a41ceaf4066ca256a8c02c98762a4f6f
bruaba <maronho16@gmail.com>
2020-11-23 21:23:52 +0100

    k-means without vizualisation

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+import matplotlib.pyplot as plt
+import pandas as pd
 from pyspark.ml.clustering import KMeans
+from pyspark.ml.feature import VectorAssembler
+from pyspark.mllib.linalg.distributed import RowMatrix
 from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
-from pyspark import SparkContext
 
 
 #start Spark Session
@@ -55,3 +58,48 @@ dataset = spark.read.csv("flag.csv", header = 'true', schema = schema)
 #Affichage de flag.csv
 dataset.show()
 dataset.printSchema()
+
+#A feature transformer that merges multiple columns into a vector column.
+vecAssembler = VectorAssembler(
+		inputCol = ['BARS',
+		'STRIPES',
+		'COLOURS',
+		'RED',
+		'GREEN',
+		'BLUE',
+		'GOLD',
+		'WHITE',
+		'BLACK',
+		'ORANGE',
+		'MAINHUE',
+		'CIRCLES',
+		'CROSSES',
+		'SALTIRES',
+		'QUARTERS',
+		'SUNSTARS',
+		'CRESCENT',
+		'TRIANGLE',
+		'ICON',
+		'ANIMATE',
+		'TEXT',
+		'BOTRIGHT'],
+		outputCol = "features")
+
+flag_with_features = vecAssembler.transform(dataset)
+
+# Do K-means
+k = 3 
+
+kmeans_algo = KMeans().setK(k).setSeed(1).setFeaturesCol("features")
+model = kmeans_algo.fit(flag_with_features)
+centers = model.clusterCenters()
+
+# Assign clusters to flowers
+flag_with_clusters = model.transform(flag_with_features)
+
+print("Centers", centers)
+
+# Convert Spark Data Frame to Pandas Data Frame
+flag_for_viz = flag_with_clusters.toPandas()
+
+# Vizualize
