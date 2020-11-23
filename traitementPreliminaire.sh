6e3f97435a690f44119e134698058fe8b44de787
bruaba <maronho16@gmail.com>
2020-11-23 11:14:56 +0100

    change the colors in numbers to produce a flag.csv with the file traitementPreliminaire.sh

--- traitementPreliminaire.sh
+++ traitementPreliminaire.sh
@@ -5,13 +5,21 @@ awk 'BEGIN{
 		FS=OFS=","
 	} 
 	{
-		n = $1
-		n = n "," $2
+		n = $8
 
-		for (i = 8; i <= (NF); i++) {
+		for (i = 9; i <= (NF); i++) {
 			n = n ","$i
 		}
 
 		print n
 
-	}' flag.data > flag.csv
+	}' OriginData/flag.data > flag.csv
+
+sed -i 's/green/1/g' flag.csv
+sed -i 's/blue/2/g' flag.csv
+sed -i 's/gold/3/g' flag.csv
+sed -i 's/white/4/g' flag.csv
+sed -i 's/orange/5/g' flag.csv
+sed -i 's/red/6/g' flag.csv
+sed -i 's/black/7/g' flag.csv
+sed -i 's/brown/8/g' flag.csv
